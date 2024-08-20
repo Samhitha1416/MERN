58953739f8f82db83f09296e7955ae6ca5be6c2f
Samhitha1416 <guntisamhitha16@gmail.com>
2024-08-20 07:08:02 +0530

    initial commit

--- CBIT/server/routes/students.js
+++ CBIT/server/routes/students.js
@@ -16,21 +16,6 @@ router.get('/', async(req,res) =>
     }
 })
 
-router.get('/:id', async(req,res) => 
-{
-    try
-    {
-           const student = await Student.findById(req.params.id)
-           res.json(student)
-    }
-
-catch(err)
-{
-        res.send('Error ' + err)
-    }
-})
-
-
 router.post('/', async(req,res) => 
 {
     const student = new Student
@@ -38,8 +23,8 @@ router.post('/', async(req,res) =>
         name: req.body.name,
         tech: req.body.tech,
         sub: req.body.sub,
-        rollno: req.body.rollno,
-        branch: req.body.branch
+        year: req.body.year,
+        age:req.body.age
     })
 
     try
@@ -53,14 +38,14 @@ catch(err)
     }
 })
 
-router.patch('/:id',async(req,res)=> 
+router.delete('/:id',async(req,res)=> 
 {
     try
    {
         const student = await Student.findById(req.params.id) 
-        student.sub = req.body.sub
-        const a1 = await student.save()
-        res.json(a1)   
+        if (!student) return res.status(404).json({message: 'Student not found'});
+        await student.deleteOne();
+        res.json({message: 'Student Deleted'});  
     }
 catch(err)
    {
@@ -68,15 +53,30 @@ catch(err)
     }
 
 })
+router.patch('/:id',async(req,res)=> 
+    {
+        try
+       {
+        const student = await Student.findById(req.params.id);
+        
+        if (!student) {
+            return res.status(404).json({ message: 'Student not found' });
+        }
+
+        // Iterate over the request body keys and update the corresponding student fields
+        Object.keys(req.body).forEach(key => {
+            student[key] = req.body[key];
+        });
+
+        const updatedStudent = await student.save();
+        res.json(updatedStudent);
+   
+        }
+    catch(err)
+       {
+            res.send('Error')
+        }
+    
+    })
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
