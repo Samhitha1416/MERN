e0a9dc87de6d2fca52ff652cd100126aa054d354
Samhitha1416 <guntisamhitha16@gmail.com>
2024-08-22 11:55:58 +0530

    second commit

--- CBIT/server/app.js
+++ CBIT/server/app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 //const cors=require('cors')
 const studentRouter = require('./routes/students')
 
-
-const url = 'mongodb://127.0.0.1:27017/CBIT'
+const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url = 'mongodb://127.0.0.1:27017/CBIT'
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
