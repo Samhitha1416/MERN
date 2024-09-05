486b12c9151aaaacaf4e97851d21c610b65e46d6
Samhitha1416 <guntisamhitha16@gmail.com>
2024-09-05 16:53:47 +0530

    React

--- CBIT/server/app.js
+++ CBIT/server/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-//const cors=require('cors')
+const cors=require('cors')
 const studentRouter = require('./routes/students')
 
 //below is the cloud db
 const url = 'mongodb+srv://guntisamhitha16:8985080910@merncrud.pxurr.mongodb.net/?retryWrites=true&w=majority&appName=MERNCRUD';
 //const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
-//the below one is the first url used
 //const url = 'mongodb://127.0.0.1:27017/CBIT'
 const app = express()
 mongoose.connect(url)
@@ -17,7 +16,7 @@ con.on('open', () =>
 {
 console.log('connected...')
 })
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.use('/students',studentRouter)
